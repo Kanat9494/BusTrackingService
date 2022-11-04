bc9a4095cc40170d714726e7c5bb30fe2379c5b3
kana2022 <kkanat94@gmail.com>
2022-11-04 12:42:45 +0600

    Added several versions of use

--- BusTrackingService/Hubs/TrackHub.cs
+++ BusTrackingService/Hubs/TrackHub.cs
@@ -5,5 +5,13 @@ public class TrackHub : Hub
     public async Task SendBusTrack(int id, string busNumber, decimal latitude, decimal longitude)
     {
         await Clients.Client(Context.ConnectionId).SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
+
+        // Расскомментировать в случае нужды или тестирования
+
+        //await Clients.All.SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
+        //await Clients.AllExcept(new List<string> { Context.ConnectionId }).SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
+        //await Clients.Others.SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
+
+        // Конец фрагмента
     }
 }
