7850a207239e9785a1200b71a65baf4454191ed7
kana2022 <kkanat94@gmail.com>
2022-11-04 12:32:12 +0600

    changed hub

--- BusTrackingService/Hubs/TrackHub.cs
+++ BusTrackingService/Hubs/TrackHub.cs
@@ -4,6 +4,6 @@ public class TrackHub : Hub
 {
     public async Task SendBusTrack(int id, string busNumber, decimal latitude, decimal longitude)
     {
-        await Clients.All.SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
+        await Clients.Client(Context.ConnectionId).SendAsync("ReceiveBusTrack", id, busNumber, latitude, longitude);
     }
 }
